be784eba6cddabbeae2d4494e5122bfc5583fb5d
mujtaba imad <mujtabaimad8@gmail.com>
2019-04-05 21:18:34 +0200

    first push it may contains some errors

--- models/models.py
+++ models/models.py
@@ -1,15 +1,32 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
-# class demo(models.Model):
-#     _name = 'demo.demo'
+class Equipment (models.Model):
+    
+    _inherit = 'maintenance.equipment.category'
+    reigon_no = fields.Integer()
+    reference_no = fields.Text()
+    Model_no = fields.Integer()
+    serial_no = fields.Text()
+    no_of_working_hours = fields.Integer()
+    well_no = fields.Integer()
+    Equipment_location = fields.Many2one('company.locations','location_name')
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class company_locations (models.Model):
+    _name = 'company.locations'
+    location_name = fields.Text()
+
+class maintenance (models.Model):
+    
+    _inherit = ['maintenance.request']
+    maintenance_spare_parts = fields.Many2one('location_inventory','spare_parts')
+
+class location_inventory (models.Model):
+    _name = 'location_inventory'
+    spare_parts = fields.Text()
+    qty_spare_part = fields.Integer()
+    inventory_location = fields.Many2one('company.locations','location_name')
+
+
+    
+
